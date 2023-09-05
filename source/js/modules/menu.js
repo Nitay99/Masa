a3aa341c9e4d35baa318a117081b83635dc6b19c
Nitay <trakktttor@gmail.com>
2023-09-05 21:49:18 +0700

    Завершает бургер-меню

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -1,7 +1,15 @@
 const menu = document.querySelector('.dropdown-menu');
 const checkbox = menu ? menu.querySelector('input') : false;
+const menuLinks = menu ? menu.querySelectorAll('a') : false;
+const overlay = document.querySelector('.overlay');
+
+if (menu) {
+  menu.classList.remove('dropdown-menu--nojs');
+}
 
 if (checkbox) {
+  checkbox.classList.remove('dropdown-menu__checkbox--nojs');
+
   checkbox.addEventListener('keydown', (evt) => {
     if (evt.key === 'Enter') {
       if (checkbox.checked) {
@@ -10,5 +18,19 @@ if (checkbox) {
         checkbox.checked = true;
       }
     }
+  });
+}
+
+if (overlay && checkbox) {
+  overlay.addEventListener('click', function () {
+    checkbox.checked = false;
+  });
+}
+
+if (menuLinks.length >= 1 && menu && checkbox) {
+  menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener('click', function () {
+      checkbox.checked = false;
+    });
   });
 }
