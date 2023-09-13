264ed20e71a82615e7742f45fdd7a7b84a9d30ca
Nitay <trakktttor@gmail.com>
2023-09-13 21:58:14 +0700

    Делает блок "Программы"

--- source/js/modules/sliders/program-slider.js
+++ source/js/modules/sliders/program-slider.js
@@ -1,38 +1,51 @@
 // eslint-disable-next-line
-import Swiper, {Pagination} from '../../vendor/swiper';
+import Swiper, {Navigation} from '../../vendor/swiper';
 
 const programSlider = document.querySelector('.programs__slider');
+const lines = document.querySelectorAll('.slider-controls__line');
 
 const initProgramSlider = () => {
-  Swiper.use([Pagination]);
+  Swiper.use([Navigation]);
   // eslint-disable-next-line
   const swiper = new Swiper(programSlider, {
     // Optional parameters
     direction: 'horizontal',
-    loop: true,
+    loop: false,
 
-    // If we need pagination
-    // pagination: {
-    //   el: '.swiper-pagination',
-    //   clickable: true,
-    // },
+    navigation: {
+      nextEl: '.programs__button-next',
+      prevEl: '.programs__button-prev',
+    },
+
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+    },
 
     speed: 250,
 
     breakpoints: {
       // when window width is >= 320px
       320: {
+        slidesPerView: 1,
         spaceBetween: 20,
         allowTouchMove: true,
       },
       768: {
-        spaceBetween: 20,
+        slidesPerView: 2.13,
+        spaceBetween: 30,
         allowTouchMove: true,
+        scrollbar: {
+          dragSize: 324,
+        },
       },
       1200: {
         slidesPerView: 3,
         spaceBetween: 32,
         allowTouchMove: false,
+        scrollbar: {
+          dragSize: 392,
+        },
       },
     },
   });
